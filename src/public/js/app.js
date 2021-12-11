ad04b3e58f3395a56366d0ef2fb2feef85de195e
JWooni <wjddns59@naver.com>
2021-12-11 19:42:34 +0900

    commit

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,18 @@
 const socket = io();
+const myFace = document.querySelector("#myFace");
+
+let myStream;
+
+async function getMedia() {
+  try {
+    myStream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: true,
+    });
+    console.log(myStream);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+getMedia();
