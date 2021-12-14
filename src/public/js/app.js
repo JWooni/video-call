bc65c6000c2a4c82c48646d61e9187fa5aa52d87
JWooni <wjddns59@naver.com>
2021-12-14 21:55:38 +0900

    commit

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -8,10 +8,22 @@ let muted = false;
 let cameraOff = false;
 
 async function getCameras() {
-	try{
-		const devices = await navigator.mediaDevices.enumerateDevices();
-		console.log(devices);
-	}
+  try {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const cameras = devices.filter((device) => device.kind === "videoinput");
+    const currentCamera = myStream.getVideoTracks()[0];
+    cameras.forEach((camera) => {
+      const option = document.createElement("option");
+      option.value = camera.deviceId;
+      option.innerText = camera.label;
+      if (currentCamera.label === camera.label) {
+        option.selected = true;
+      }
+      camerasSelect.appendChild(option);
+    });
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 async function getMedia() {
